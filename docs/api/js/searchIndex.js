739ddba3797630c41a7885716fd5c97774fc22d5
Noelia Navas <desarrolloappweb@gmail.com>
2023-08-23 19:05:10 +0200

    Mi MapaEuropa

---
+++ docs/api/js/searchIndex.js
@@ -0,0 +1,14 @@
+Search.appendIndex(
+    [
+                {
+            "fqsen": "\\map_shortcode\u0028\u0029",
+            "name": "map_shortcode",
+            "summary": "Mapa\u0020Europa",
+            "url": "namespaces/default.html#function_map_shortcode"
+        },                {
+            "fqsen": "\\",
+            "name": "\\",
+            "summary": "",
+            "url": "namespaces/default.html"
+        }            ]
+);
